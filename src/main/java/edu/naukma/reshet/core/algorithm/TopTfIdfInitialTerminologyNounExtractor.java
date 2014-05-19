6065567a56bdf7d86d0673782cebcba3b8e98ec4
Ihor Reshetnov <ihor.reshetnov@get.no>
2014-05-19 13:24:40 +0200

    Noun extractor on initial step  tested.

---
+++ src/main/java/edu/naukma/reshet/core/algorithm/TopTfIdfInitialTerminologyNounExtractor.java
@@ -0,0 +1,78 @@
+package edu.naukma.reshet.core.algorithm;
+
+import com.carrotsearch.ant.tasks.junit4.dependencies.com.google.common.collect.Lists;
+import com.carrotsearch.ant.tasks.junit4.dependencies.com.google.common.collect.Ordering;
+import edu.naukma.reshet.model.TermInDoc;
+import edu.naukma.reshet.repositories.TerminRepository;
+import edu.naukma.reshet.shared.DocumentaryFrequencyCrawler;
+import edu.naukma.reshet.shared.Searcher;
+import edu.naukma.reshet.shared.algorithm.InitialTerminologyExtractor;
+import org.languagetool.AnalyzedSentence;
+import org.languagetool.AnalyzedTokenReadings;
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.Ukrainian;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class TopTfIdfInitialTerminologyNounExtractor implements InitialTerminologyExtractor {
+
+  @Autowired
+  private
+  DocumentaryFrequencyCrawler crawler;
+
+  @Autowired
+  private
+  TerminRepository terminRepo;
+
+  private JLanguageTool langTool;
+
+  public TopTfIdfInitialTerminologyNounExtractor(){
+      try {
+          langTool = new JLanguageTool(new Ukrainian());
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+  @Override
+  public List<TermInDoc> extractValuableTerms(Searcher searcher, Integer docId) {
+    List<TermInDoc> terms = Lists.newArrayList();
+    Map<String, Integer> map = searcher.getFrequencies(docId);
+    for(String term: map.keySet()){
+        try {
+            List<AnalyzedSentence> analyzed = langTool.analyzeText(term);
+            AnalyzedSentence sentence = analyzed.get(0);
+            boolean hasNoun = sentence.getTokensWithoutWhitespace()[1].hasPartialPosTag("noun");
+
+            AnalyzedTokenReadings[] tokens = analyzed.get(0).getTokensWithoutWhitespace();
+            System.out.print(tokens);
+
+            if (hasNoun) {
+                Integer frequency =  map.get(term);
+                Long docFreq = crawler.getDocumentaryFrequency(term);
+                Double totalFreq = 1.0*frequency/docFreq;
+                terms.add(new TermInDoc(terminRepo.findByText(term),totalFreq));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    return Ordering
+            .natural()
+            .reverse()
+            .immutableSortedCopy(terms);
+  }
+
+    public void setCrawler(DocumentaryFrequencyCrawler crawler) {
+        this.crawler = crawler;
+    }
+
+    public void setTerminRepo(TerminRepository terminRepo) {
+        this.terminRepo = terminRepo;
+    }
+}
---
+++ src/test/java/edu/naukma/reshet/core/algorithm/NounExtractorTest.java
@@ -0,0 +1,74 @@
+package edu.naukma.reshet.core.algorithm;
+
+import com.carrotsearch.ant.tasks.junit4.dependencies.com.google.common.collect.Maps;
+import edu.naukma.reshet.core.Algorithm;
+import edu.naukma.reshet.model.TermInDoc;
+import edu.naukma.reshet.model.Termin;
+import edu.naukma.reshet.repositories.TerminRepository;
+import edu.naukma.reshet.shared.DocumentaryFrequencyCrawler;
+import edu.naukma.reshet.shared.Searcher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.refEq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+public class NounExtractorTest {
+    @Mock
+    DocumentaryFrequencyCrawler crawler;
+
+    @Mock
+    TerminRepository terminRepo;
+
+    @InjectMocks
+    TopTfIdfInitialTerminologyNounExtractor extractor = new TopTfIdfInitialTerminologyNounExtractor();
+
+    @Test
+    public void noun_extractor_test(){
+        Searcher searcher = mock(Searcher.class);
+        Map<String, Integer> termFrequencies = new LinkedHashMap<>();
+        termFrequencies.put("і",1);
+        termFrequencies.put("або",1);
+        termFrequencies.put("термін",1);
+        termFrequencies.put("соціологія",1);
+        termFrequencies.put("яскравий",1);
+
+        when(searcher.getFrequencies(anyInt())).thenReturn(termFrequencies);
+        when(crawler.getDocumentaryFrequency(anyString())).thenReturn(100000L);
+        when(terminRepo.findByText(refEq("термін"))).thenReturn(new Termin("термін",1L));
+        when(terminRepo.findByText(refEq("соціологія"))).thenReturn(new Termin("соціологія",1L));
+        List<TermInDoc> terms = extractor.extractValuableTerms(searcher, 0);
+        assertEquals("Should be 2 noun terms in list", terms.size(), 2);
+        assertEquals("First noun term", terms.get(0).getTermin().getText(), "термін");
+        assertEquals("Second noun term", terms.get(1).getTermin().getText(), "соціологія");
+    }
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+}
+
+@Configuration
+class TestConfig{
+
+}
